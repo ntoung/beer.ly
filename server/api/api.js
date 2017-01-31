d6cb9c52f72b804273ee1f484168a8a84507f00c
michaelmerrill <michael@michaelmerrill.io>
2017-01-31 10:37:02 -0800

    Refactor Webpack

--- server/api/api.js
+++ server/api/api.js
@@ -1,3 +1,10 @@
-const router = require('express').Router();
+'use strict'
 
-module.exports = router;
+const router = require('express').Router()
+const users = require('./users/userRoutes')
+const posts = require('./posts/postRoutes')
+
+router.use('/users', users)
+router.use('/posts', posts)
+
+module.exports = router
--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,19 +1,48 @@
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const path = require('path');
+'use strict'
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const path = require('path')
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('../../webpack.config.js');
 
-module.exports = function(app, express) {
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 8008 : process.env.PORT;
 
-  // parse application/x-www-form-urlencoded
-  // for authentication
-  app.use(bodyParser.urlencoded({ extended: true }));
+// Build directory is where the bundle file will be placed
+var BUILD_DIR = path.join(__dirname, '/../../dist/');
 
-  // automatically parses stringified data
-  app.use(bodyParser.json());
+module.exports = function (app, express) {
+  app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
+  app.use(bodyParser.json()) // parse application/json
+  app.use(morgan('dev'))
 
-  // request logger
-  app.use(morgan('dev'));
+  if (isDeveloping) {
+    const compiler = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+      publicPath: config.output.publicPath,
+      contentBase: 'src',
+      stats: {
+        colors: true,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        modules: false
+      }
+    });
 
-  // serve bundled client files
-  app.use(express.static(path.join(__dirname, '/../../', 'dist')));
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.get('/', function response(req, res) {
+      res.write(middleware.fileSystem.readFileSync(path.join(BUILD_DIR, 'index.html')));
+      res.end();
+    });
+  } else {
+    app.use(express.static(BUILD_DIR));
+    app.get('/', function response(req, res) {
+      res.sendFile(path.join(BUILD_DIR, 'index.html'));
+    });
+  }
 }
