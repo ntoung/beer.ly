23f90d3fa2a1d662daebf7927e73f86407254c69
Ranit Dubey <ranitdubey@gmail.com>
2017-01-31 16:18:15 -0800

    setup row components with equal space between beer item in row

--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import BeerItem from './BeerItem.jsx';
+import styles from '../BeerList.css';
 
-var BeerList = (props) => (
-  <div>
-    {props.beers.map((beer) =>
-      <BeerItem
-        beer={beer}
-      />
-    )}
-  </div>
+const rowSize = 3;
+
+var BeerRow = (props) => (
+	<div className={styles.beerRow}>
+		{
+			props.beers.map((beer) => 
+				<BeerItem 
+					beer={beer}
+					className={styles.beerItem}
+				/>
+			)
+		}
+	</div>
 );
+
+var BeerList = (props) => {
+  //code here to build beer rows
+  var beerRows = [];
+  var curRow = [];
+  for (var i = 0; i < props.beers.length; i++) {
+  	curRow.push(props.beers[i]);
+  	if (curRow.length === rowSize) {
+  		var beerRow = (<BeerRow beers={curRow} style={styles.beerRow}/>);
+  		beerRows.push(beerRow);
+  		curRow = [];
+  	}
+  }
+ //  var beerRow = (<BeerRow beers={curRow} style={styles.beerRow}/>);
+	// beerRows.push(beerRow);
+  return (
+	  <div>
+	  	{beerRows}
+	  </div>
+	)
+};
 export default BeerList;
+
