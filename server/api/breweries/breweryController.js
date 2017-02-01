9017a368715117dac37623f23221b1b48b811639
ntoung <nmtoung@gmail.com>
2017-02-01 15:37:12 -0800

    Add axios request to breweryDB through our API endpoint

--- server/api/breweries/breweryController.js
+++ server/api/breweries/breweryController.js
@@ -4,36 +4,53 @@ const axios = require('axios');
 
 // const Post = require('./postModel')
 const _API_KEY = require('../config/apiKeys.js').breweryDBKey;
-const _API_ENDPOINT = 'http://api.brewerydb.com/v2/';
+const _API_BASEURL = 'http://api.brewerydb.com/v2/';
+
+
 
 exports.get = (req, res, next) => {
-  console.log('brewery get controller');
-  
 
-  // Post.find({})
-  // .then((post) => {
-  //   if (post) {
-  //     res.json(post)
-  //   } else {
-  //     console.log('No posts in database')
-  //   }
-  // })
+  // breweryDB endpoint
+  var endPoint = 'locations/';
+
+  // endpoint query options
+  var queryOptions = {
+    //locality: 'San Francisco'
+    locality: req.params.location,
+    p: '1'
+  };
+
+  // axios RESTful API call
+  axios.get(createUrl(endPoint, queryOptions))
+  .then(function (response) {
+    res.end(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
 }
 
 exports.post = (req, res, next) => {
-  console.log('brewery post controller');
-  // let newPost = new Post({
-  //   title: req.body.title,
-  //   text: req.body.text,
-  //   author: req.body.author
-  // })
-  //
-  // newPost.save()
-  // .then((post) => {
-  //   if (post) {
-  //     res.json(post)
-  //   } else {
-  //     console.log('Could not save post')
-  //   }
-  // })
+  // console.log('brewery post controller');
+
+}
+
+// Helper formatting function for connecting to breweryDB 
+var createUrl = function(endPoint, queryOptions) {
+  var key = '?key=' + _API_KEY;
+
+  var queryStrings = [];
+
+  // Create query string from all query options
+  for (let query in queryOptions) {
+    if (typeof queryOptions[query] === 'string') {
+      // encode spaces for url if query option is string
+      queryStrings.push(query + '=' + queryOptions[query].replace(' ', '+'));
+    } else {
+      queryStrings.push(query + '=' + queryOptions[query]);
+    }
+  }
+
+  return _API_BASEURL + endPoint + key + '&' + queryStrings.join('&');
 }
