f51d7db9abd5a825f231a4b9988785ae0d77cfe9
ntoung <nmtoung@gmail.com>
2017-02-03 11:11:37 -0800

    Add secret key in config for jwt

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,11 @@
 const config = {
   dev: 'development',
   prod: 'production',
-  port: process.env.PORT || 8008
+  port: process.env.PORT || 8008,
+  database: {
+    local: 'mongodb://localhost/beerly',
+    firebase: '' // if you want to use firebase
+  }
 };
 
 process.env.NODE_ENV = process.env.NODE_ENV || config.dev;
