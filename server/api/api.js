35a0a336b0da651ce5efb4e29a817a94cfea9366
ntoung <nmtoung@gmail.com>
2017-02-03 11:10:35 -0800

    Move user routing out of api

--- server/api/api.js
+++ server/api/api.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const router = require('express').Router();
-const users = require('./users/userRoutes');
 const posts = require('./posts/postRoutes');
 const breweries = require('./breweries/breweryRoutes');
 const locations = require('./locations/locationRoutes');
 
 
-router.use('/users', users);
 router.use('/posts', posts);
 router.use('/breweries', breweries);
 router.use('/locations', locations);
