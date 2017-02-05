dfe54ee34df8e181e17d2454e5e65a3e86144d9a
ntoung <nmtoung@gmail.com>
2017-02-04 18:04:41 -0800

    Add styling for Brewery List page

--- client/components/BreweryListItem/BreweryListItem.js
+++ client/components/BreweryListItem/BreweryListItem.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router';
 import styles from './BreweryListItem.css';
+import InfoButton from 'material-ui/IconButton';
 
 class BreweryList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const breweryInfo = this.props.brewery.brewery;
+
+    this.squareImage = (breweryInfo.images) ? breweryInfo.images.squareMedium : '';
+    this.website = breweryInfo.website;
+    this.description = breweryInfo.description;
+    this.phoneNumber = this.props.brewery.phone || '(650) 269 - 2188'; // Default number
+    this.cleanedPhoneNumber = this.phoneNumber.replace(/["'() -]/g,"");
+
+  }
+
+  // <Link href={"tel:" + this.cleanedPhoneNumber }>
+  //   <p className={styles.details}>{this.phoneNumber}</p>
+  // </Link>
+          // <Link href={this.website}>
+          //   <p className={styles.details}>{this.website}</p>
+          // </Link>
+
   render() {
     return (
-      <Link className={styles.cell} to={`/${this.props.city}/${this.props.brewery.brewery.name}`}>
-        <h2 className={styles.title}>{this.props.brewery.brewery.name}</h2>
-      </Link>
-    );
+      <div className={styles.cell} >
+        <Link className={styles.moreInfo} to="/modal">
+          <i className="material-icons">info_outline</i>
+        </Link>
+        <Link to={`/${this.props.city}/${this.props.brewery.brewery.name}`}>
+            <img className={styles.cover} src={this.squareImage} />
+        </Link>
+        <div className={styles.info}>
+          <h3 className={styles.title}>{this.props.brewery.brewery.name}</h3>
+        </div>
+      </div>
+    )
   }
 }
 
