2d38132db838a2082ea4bf24a2b760fb71dcfe4a
ntoung <nmtoung@gmail.com>
2017-02-07 11:13:36 -0800

    Create empty apiKeys.js file

---
+++ server/config/apiKeys.js
@@ -0,0 +1,9 @@
+module.exports.breweryDBKey = '';
+
+module.exports.googleMapsAPIKey = '';
+
+module.exports.stripeTestKey = '';
+
+module.exports.stripePublishableKey = '';
+
+module.exports.beerlyAuthSecret = '';
